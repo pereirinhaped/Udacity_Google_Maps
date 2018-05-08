d8ac4304274bffdf6c519aa1feb2fcc9023c7f30
Pedro Pereirinha <pereirinhaped@gmail.com>
2018-05-08 17:46:39 +0100

    Markers added

--- src/js/map.js
+++ src/js/map.js
@@ -1,21 +1,77 @@
 
-var mapId = "map";
-var mapHtmlElement = document.getElementById(mapId);
+// Map document constants
+var mapElementId = "map";
+var mapHtmlElement = document.getElementById(mapElementId);
+
+// Will hold the Map object
 var map;
+
+// Some test markers
+var reservoirs = [
+    {
+        position: {
+            lat: 38.765246,
+            lng: -9.102585
+        },
+        label: "R1",
+        title: "Reservatório Av. Berlim"
+    }, {
+        position: {
+            lat: 38.762610,
+            lng: -9.124741
+        },
+        label: "R2",
+        title: "Reservatório Olivais Sul"
+    }, {
+        position: {
+            lat: 38.750095,
+            lng: -9.114399
+        },
+        label: "R3",
+        title: "Reservatório Chelas"
+    }
+];
 
 // Map Initial Params
 var mapInitCenter = {
-    lat: 38.753548, 
-    lng: -9.144780
+    lat: 38.765246,
+    lng: -9.102585
 };
+var mapInitZoom = 15;
 
 var mapInitParams = {
     center: mapInitCenter,
-    zoom: 13
-}
+    zoom: mapInitZoom
+};
+
 
 // Map init callback
 function initMap() {
-    map = new google.maps.Map(mapHtmlElement, mapInitParams);
-    console.log("Map init callback ends.");
+    loadMap(mapHtmlElement, mapInitParams);
+    
+    loadMarkers(map, reservoirs);
+}
+
+// Google Maps API access
+function loadMarkers(map, markers) {
+    for (var i = 0; i < markers.length; i++)
+        getNewMarker(map, markers[i]);
+}
+
+function getNewMarker(map, marker) {
+    return new google.maps.Marker(
+        {
+            position: marker.position,
+            label: marker.label,
+            map: map,
+            title: marker.title
+        }
+    );
+}
+
+function loadMap(element, params) {
+    map = new google.maps.Map(
+        element, 
+        params
+    );
 }
