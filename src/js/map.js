14743a295f62247eebae567df41cf4fdc9c7d5b1
Pedro Pereirinha <pereirinhaped@gmail.com>
2018-05-10 13:00:34 +0100

    update

--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,10 @@
 
 // Map document constants
-var mapElementId = "map";
+var mapElementId = "map_canvas";
 var mapHtmlElement = document.getElementById(mapElementId);
 
 // Will hold the Map object
-var map;
+var myMap;
 
 // Some test markers
 var reservoirs = [
@@ -41,36 +41,75 @@ var mapInitZoom = 15;
 
 var mapInitParams = {
     center: mapInitCenter,
-    zoom: mapInitZoom
+    zoom: mapInitZoom,
+    mapTypeControl: false
 };
 
+var markers = [];
 
 // Map init callback
 function initMap() {
     loadMap(mapHtmlElement, mapInitParams);
     
-    loadMarkers(map, reservoirs);
+    loadGeoDataToMarkersArray(markers, reservoirs);
+
+    document.getElementById("show-reservoirs").addEventListener("click", showMarkers);
+    document.getElementById("hide-reservoirs").addEventListener("click", hideMarkers);
 }
 
 // Google Maps API access
-function loadMarkers(map, markers) {
-    for (var i = 0; i < markers.length; i++)
-        getNewMarker(map, markers[i]);
+function showMarkers() {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(myMap);
+        bounds.extend(markers[i].position);
+    }
+    myMap.fitBounds(bounds);
 }
 
-function getNewMarker(map, marker) {
+function hideMarkers() {
+    for (var i = 0; i < markers.length; i++)
+        markers[i].setMap(null);
+}
+
+function loadGeoDataToMarkersArray(markersArr, reservoirs) {
+    var infowindow = new google.maps.InfoWindow();
+    for (var i = 0; i < reservoirs.length; i++) {
+        markersArr.push( getNewMarker( reservoirs[i], "R" + i ));
+        markersArr[i].addListener(
+            'click', function() {
+                populateInfoWindow(this, infowindow)
+            }
+        );
+    }
+}
+
+function getNewMarker(marker, id) {
     return new google.maps.Marker(
         {
             position: marker.position,
             label: marker.label,
-            map: map,
-            title: marker.title
+            title: marker.title,
+            id: id
         }
     );
 }
 
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(myMap, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
+  }
+
 function loadMap(element, params) {
-    map = new google.maps.Map(
+    myMap = new google.maps.Map(
         element, 
         params
     );
